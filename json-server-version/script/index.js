49960595cea9cbd56985748b6b4db79ba23767fb
André Feuzer <andre.heydran@gmail.com>
2022-06-03 18:33:57 -0300

    just update

--- json-server-version/script/index.js
+++ json-server-version/script/index.js
@@ -1,6 +1,22 @@
 var logado = localStorage.getItem("Logado")
 console.log(logado);
-if(logado == "true"){
+if (logado == "true") {
     document.getElementById("aUser").innerHTML = localStorage.getItem("Usuario")
+    configLogado()
+}
 
+function configLogado() {
+    document.getElementById("liCadastroJogo").classList.remove("esconder")
+    document.getElementById("liUsuario").classList.remove("esconder")
+    document.getElementById("liLogout").classList.remove("esconder")
+    document.getElementById("liLogin").classList.add("esconder")
+}
+
+function logout() {
+    localStorage.removeItem("Usuario")
+    localStorage.setItem("Logado", "false")
+    document.getElementById("liCadastroJogo").classList.add("esconder")
+    document.getElementById("liUsuario").classList.add("esconder")
+    document.getElementById("liLogout").classList.add("esconder")
+    document.getElementById("liLogin").classList.remove("esconder")
 }
--- json-server-version/script/login.js
+++ json-server-version/script/login.js
@@ -2,14 +2,15 @@ function logar() {
     user = document.getElementById("user")
     pass = document.getElementById("pass")
     fetch("http://localhost:3000/contas")
-    .then(accs => accs.json())
-    .then(jsonAccs => {
-        for (i of jsonAccs){
-            if (user.value == i.user & pass.value == i.pass) {
-                localStorage.setItem("Logado","true")
-                localStorage.setItem("Usuario",user.value)
-
+        .then(accs => accs.json())
+        .then(jsonAccs => {
+            for (i of jsonAccs) {
+                if (user.value == i.user & pass.value == i.pass) {
+                    localStorage.setItem("Logado", "true")
+                    localStorage.setItem("Usuario", user.value)
+                    parent.location.reload()
+                    parent.document.getElementById("mainFrame").src = "home.html"
+                }
             }
-        }
-    })
+        })
 }
