f007f99f029393be6e38eb9423972e17646574bb
André Feuzer <andre.heydran@gmail.com>
2022-06-09 21:46:09 -0300

    Atualização da biblioteca Steam

--- json-server-version/script/bibliotecaSteam.js
+++ json-server-version/script/bibliotecaSteam.js
@@ -1,44 +1,26 @@
 carregarJogos()
 
 function carregarJogos() {
-
-    fetch("http://localhost:3000/games")
+    fetch("http://localhost:3333/jogos")
         .then(jsonString => jsonString.json())
         .then(jsonObject => {
-            for (i of jsonObject) {
-                escolherBaner(i)
+            for (jogo of jsonObject.jogos) {
+                escolherBaner(jogo);
             }
         })
 }
 
+
 function escolherBaner(jogo) {
-    var banner = document.getElementById(jogo.generoPrincipal)
-    if (!banner) {
-        document.getElementById("ulBilioteca").innerHTML += `
-            <li class="nav-item">
-                <a class="nav-link text-info" href="#div${jogo.generoPrincipal}" target="mainFrame">${jogo.generoPrincipal}</a>
-            </li>`;
-        document.getElementById("mural").innerHTML += `
-        <div id="div${jogo.generoPrincipal}">
-            <h1 ">${jogo.generoPrincipal}</h1>
-            <div id="${jogo.generoPrincipal}" class="container-fluid bg1 banners">
-            </div>
-            </div>`;
-
-
-        var banner = document.getElementById(jogo.generoPrincipal)
-    }
-    /*banner.addEventListener("wheel", (evt) => {
-        evt.preventDefault();
-        banner.scrollLeft += evt.deltaY;
-    });*/
+    var banner = document.getElementById("jogos")
     adicionarJogo(jogo, banner)
 }
 
 function adicionarJogo(jogo, banner) {
     banner.innerHTML +=
         `<div class="contImg">
-            <img id="img-${jogo.id}" src="${jogo.linkImg}" alt="<img src='static/img/NOTFOUNDED.png'>">
+            <img id="img-${jogo.nome}" src="${jogo.link}">
         </div>`
+
 
 }
--- json-server-version/script/testeSteamAPI.js
+++ json-server-version/script/testeSteamAPI.js
@@ -1,8 +1,43 @@
-fetch("https://store.steampowered.com/api/appdetails?appids=105600&key=CDD51DAFA10A6B45939EB4EF42952371format=json", {
-        mode: 'no-cors'
-    })
-    .then(jsonString => (JSON.stringify(jsonString)))
+/*fetch("https://store.steampowered.com/api/appdetails?
+appids=105600&key=CDD51DAFA10A6B45939EB4EF42952371format=json", {})
+    .then(jsonString => ((jsonString)))
     .then(jsonObject => {
         console.log(jsonObject)
 
-    })
+    })*/
+
+
+
+var express = require('express');
+var app = express();
+const port = 3333
+
+var cors = require('cors')
+app.use(cors())
+
+const SteamAPI = require('steamapi');
+const steam = new SteamAPI('CDD51DAFA10A6B45939EB4EF42952371');
+
+// respond with "hello world" when a GET request is made to the homepage
+
+var jogos = []
+app.get('/jogos', (req, res) => {
+    steam.getUserOwnedGames("76561198271916709")
+        .then(jsonObject => {
+            jogos = []
+
+            for (jogo of jsonObject) {
+                console.log(jogo);
+                jogos.push({
+                    nome: jogo.name,
+                    link: `https://cdn.akamai.steamstatic.com/steam/apps/${jogo.appID}/header.jpg`
+                })
+            }
+
+        }).then(e => res.send({ jogos: jogos }))
+
+
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
